00da1786cb1eaa0167f8fb784e1cd795b6643081
KimiChen <jianxieshui@gmail.com>
2015-01-26 19:36:59 +0800

    update for iOS 8.x

--- iOS/install.sh
+++ iOS/install.sh
@@ -12,11 +12,11 @@ cp rsa.bsms /usr/libexec/cydia/rsa.bsms
 chmod 600 /usr/libexec/cydia/rsa.bsms
 chown root:wheel /usr/libexec/cydia/rsa.bsms
 
-cp com.bsms.bsms.plist /System/Library/LaunchDaemons/com.bsms.bsms.plist
-chmod 644 /System/Library/LaunchDaemons/com.bsms.bsms.plist
-chown root:wheel /System/Library/LaunchDaemons/com.bsms.bsms.plist
+cp com.bsms.bsms.plist /Library/LaunchDaemons/com.bsms.bsms.plist
+chmod 644 /Library/LaunchDaemons/com.bsms.bsms.plist
+chown root:wheel /Library/LaunchDaemons/com.bsms.bsms.plist
 
 ls -al /usr/libexec/cydia/bsms
 ls -al /usr/libexec/cydia/cacert.bsms
 ls -al /usr/libexec/cydia/rsa.bsms
-ls -al /System/Library/LaunchDaemons/com.bsms.bsms.plist
+ls -al /Library/LaunchDaemons/com.bsms.bsms.plist
